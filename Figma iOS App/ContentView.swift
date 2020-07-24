725beaca22d77bf89c4e0b3d579904713b7070f7
Anatoly <verbitskii@yamoney.ru>
2020-07-24 22:13:46 +0300

    Initial Commit

---
+++ Figma iOS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Figma iOS App
+//
+//  Created by Анатолий . Вербицкий on 24.07.2020.
+//  Copyright © 2020 lonewolfkaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
